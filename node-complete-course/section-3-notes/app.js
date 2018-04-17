44febfa78e678159cdfef1a4e04c8805992be050
Jordan <jordandick@gmail.com>
2018-04-17 21:48:07 +0100

    Notes JS

---
+++ node-complete-course/section-3-notes/app.js
@@ -0,0 +1,23 @@
+console.log('Starting App.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+
+const notes = require('./notes');
+
+let result = notes.addNote();
+console.log(`${result}`)
+
+result = notes.add(5,6);
+console.log(`should be 11 === ${result}`);
+
+let dups = [1,2,3,4,5,4,3,7,8];
+let noDups = _.uniq(dups);
+console.log(noDups);cd ..
+    
+//
+//
+// let user = os.userInfo().username;
+//
+// fs.appendFileSync('greetings.txt', `Hello ${user} You are ${notes.age}`  );
